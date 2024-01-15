f69109284de07e41261d33a4bf6c82eeb856a253
LarissaLima97 <larissa.lmaria@gmail.com>
2024-01-15 18:28:47 -0300

    Creating menu

--- Personagem.ts
+++ Personagem.ts
@@ -1,3 +1,4 @@
+import prompt from 'prompt-sync';
 class Personagem {
     constructor(
         public nome: string, 
@@ -11,4 +12,30 @@ class Personagem {
 let tony: Personagem = new Personagem('Tony', 60,80,70,65);
 
 let hulk: Personagem = new Personagem('Hulk',80,880,100,40);
+
+let teclado = prompt();
+let option:number = 0; 
+
+while(option !== 9) {
+    console.log('*********Personagem*********');
+    console.log('|1.Treinar ataque          |');
+    console.log('|2.Treinar defesa          |');
+    console.log('|3.Imprimir atributos      |');
+    console.log('|9.Sair                    |');
+    console.log('****************************');
+
+    option = +teclado('Escolha uma ação: ');
+    
+    switch(option) {
+        case 1:
+            tony.ataque += 2
+            break;
+        case 3:
+            console.log(tony)
+            break;
+        
+        case 9: 
+            break;
+    }
+}
 
