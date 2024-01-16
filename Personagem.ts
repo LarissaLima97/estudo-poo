c7f8d0d9b3951ec8154927ef0e110c9a07ef8e87
LarissaLima97 <larissa.lmaria@gmail.com>
2024-01-16 16:43:29 -0300

    WIP:Adding methods to the Personagem class

--- Personagem.ts
+++ Personagem.ts
@@ -1,41 +1,56 @@
-import prompt from 'prompt-sync';
-class Personagem {
+export class Personagem {
     constructor(
         public nome: string, 
-        public energia: number, 
-        public vida: number, 
+        public energia: number,  
         public ataque: number, 
         public defesa: number
         ){}
+// TODA VEZ QUE PENSAR EM LER DADOS NA CLASSE - USE PARÂMETRO
+// TODA VEZ QUE PENSAR EM IMPRIMIR NA CLASSE - USE RETORNO 
+        status(): void {
+            console.log('+++++Personagem+++++');
+            console.log('Nome: ', this.nome);
+            console.log('Energia: ', this.energia);
+            console.log('Ataque: ', this.ataque);
+            console.log('Defesa: ', this.defesa);
+        }
+
+        treinarAtaque(): void {
+            this.ataque += Math.floor(Math.random() * 7);
+            this.energia -= Math.floor(Math.random() * 10);
+            this.isDead();
+            if(this.ataque >= 100){
+                this.ataque = 100; 
+                console.log('Seu ataque atingiu o nível máximo!');
+            }
+        }
+
+        treinarDefesa(): void {
+            this.defesa += Math.floor(Math.random() * 5);
+            this.energia -= Math.floor(Math.random() * 10);
+            this.isDead();
+            if(this.defesa >= 100){
+                this.defesa = 100; 
+                console.log('Sua defesa atingiu o nível máximo!');
+            }
+        }
+
+        descansar(): void {
+            this.energia += Math.floor(Math.random() * 5);
+            if(this.energia >= 100){
+                this.energia = 100; 
+                console.log('Sua energia atingiu o nível máximo!');
+            }
+        }
+
+        batalhar(): void {
+            this.isDead();
+            this.energia -= Math.floor(Math.random() * 100);
+        }
+
+        isDead(): boolean {
+            return this.energia < 0; 
+        }
 }
 
-let tony: Personagem = new Personagem('Tony', 60,80,70,65);
-
-let hulk: Personagem = new Personagem('Hulk',80,880,100,40);
-
-let teclado = prompt();
-let option:number = 0; 
-
-while(option !== 9) {
-    console.log('*********Personagem*********');
-    console.log('|1.Treinar ataque          |');
-    console.log('|2.Treinar defesa          |');
-    console.log('|3.Imprimir atributos      |');
-    console.log('|9.Sair                    |');
-    console.log('****************************');
-
-    option = +teclado('Escolha uma ação: ');
-    
-    switch(option) {
-        case 1:
-            tony.ataque += 2
-            break;
-        case 3:
-            console.log(tony)
-            break;
-        
-        case 9: 
-            break;
-    }
-}
 
