d83a5eb07b49b1f2a75b51785b4946beb977df6d
LarissaLima97 <larissa.lmaria@gmail.com>
2024-01-16 16:45:09 -0300

    WIP:Creating a file to use the Personagem class

---
+++ main.ts
@@ -0,0 +1,49 @@
+import prompt from 'prompt-sync';
+import { Personagem } from "./Personagem";
+
+let tony: Personagem = new Personagem('Tony', 60,80,70);
+
+let hulk: Personagem = new Personagem('Hulk',80,880,100);
+
+let teclado = prompt();
+let option:number = 0; 
+
+while(option !== 9 || tony.isDead()) {
+    console.log('*********Personagem*********');
+    console.log('|1.Treinar ataque          |');
+    console.log('|2.Treinar defesa          |');
+    console.log('|3.Imprimir atributos      |');
+    console.log('|4.Descansar               |');
+    console.log('|5.Batalhar                |');
+    console.log('|9.Sair                    |');
+    console.log('****************************');
+
+    option = +teclado('Escolha uma ação: ');
+    
+    switch(option) {
+        case 1:
+            tony.status();
+            tony.treinarAtaque();
+            break;
+
+        case 2:
+            tony.status();
+            tony.treinarDefesa();
+            break;
+        case 3:
+            tony.status();
+            break;
+        
+        case 4:
+            tony.status();
+            tony.descansar();
+            break;
+        case 5:
+            tony.status();
+            tony.batalhar();
+            break;
+        
+        case 9: 
+            break;
+    }
+}
