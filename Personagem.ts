42ae05af5f608e99a14f764abf10bb94a8732fd5
LarissaLima97 <larissa.lmaria@gmail.com>
2024-01-18 17:45:01 -0300

    Removing blank spaces

--- Personagem.ts
+++ Personagem.ts
@@ -13,11 +13,10 @@ export class Personagem {
             return this._nome; 
         }
 
-        
         public set nome(nome : string) {
             this._nome = nome;
         }
-        
+
         public status(): string {
             return(
                 '\n+++++Personagem+++++ \n' +
